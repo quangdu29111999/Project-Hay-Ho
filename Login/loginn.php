3c864b572cdcff1f495c6b71c2ad23dd8a996237
ducanh13 <129024223+ducanh13@users.noreply.github.com>
2023-03-31 12:44:45 +0700

    Add files via upload

---
+++ Login/loginn.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="matkhau.js">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+
+<body>
+    
+
+<section>
+    
+     <div class="img-bg">
+         <img src="https://tse2.mm.bing.net/th?id=OIP.RtmXnUuanRlPw5Yyfjw7PwHaHy&pid=Api&P=0" alt="Hình Ảnh Minh Họa">
+     </div>
+     
+   
+     <div class="noi-dung">
+         <div class="form">
+             <h2>Trang Đăng Nhập</h2>
+             <form action="">
+                 <div class="input-form">
+                     <span>Tên Người Dùng</span>
+                     <input type="text" name="">
+                 </div>
+                 <div class="input-form">
+                     <span>Mật Khẩu</span>
+                     <input type="password" name="">
+                     <span class="show-btn"><i class="fas fa-eye"></i></span>
+                 </div>
+                 <div class="nho-dang-nhap">
+                     <label><input type="checkbox" name=""> Nhớ Đăng Nhập</label>
+                     
+                 </div>
+                 <div class="input-form">
+                     <input type="submit" value="Đăng Nhập">
+                 </div>
+                 <div class="input-form">
+                     <p>Bạn Chưa Có Tài Khoản? <a href="http://localhost/php/dangki.php#">Đăng kí</a></p>
+                 </div>
+             </form>
+             <h3>Đăng Nhập Bằng Mạng Xã Hội</h3>
+             <ul class="icon-dang-nhap">
+                 <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
+                 <li><i class="fa fa-google" aria-hidden="true"></i></li>
+                 <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+             </ul>
+         </div>
+     </div>
+    
+ </section>
+ 
+</body>
+<script src="matkhau.js"></script>
+</html>
