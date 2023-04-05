05e59912767218afc46ed1def9342559da7c42dc
ducanh13 <129024223+ducanh13@users.noreply.github.com>
2023-04-05 10:43:39 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = '$'; // đơn vị tiền tệ
+$db_username = 'root'; // user name
+$db_password = 'ducanh111';// password
+$db_name = 'demo'; // tên cơ sở dữ liệu
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
